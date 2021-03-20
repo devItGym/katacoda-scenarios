aa079fae542c636a20ebadb01adc67002e28b712
Vedu Hariths <dev@theitgym.com>
2021-03-20 18:36:26 -0400

    trying to copy via shell cmd from step folders to root  dir

--- functionsrx/loadStep1.sh
+++ functionsrx/loadStep1.sh
@@ -1,12 +1,14 @@
 echo "Started"
 
-cat << 'EOF' > /root/multiply.js
+cp /root/assets/step1/*.sh /root/
+
+cat << 'EOF' > /root/step1/multiply.js
 function multiply(a, b){
   a * b
 }
 EOF
 
-cat << 'EOF' > /opt/test.js
+cat << 'EOF' > /opt/step1/test.js
 var x = multiply(2, 2);
 if(x === 4) {
   process.exit(0);
